e6c22bb14400b009290b025ffa3b6d4853349cf9
KyleM021199 <kylem021199@gmail.com>
2023-04-28 20:14:46 -0500

    added to README questions array

--- index.js
+++ index.js
@@ -3,13 +3,33 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is your Repo name?", 
+"Enter your description", 
+"Enter install instructions",
+"Enter Usage information", 
+"Enter contribution guidelines", 
+"Choose a repo license",
+"Enter test instructions",
+"Enter Github name",
+"Enter email address"];
+
+inquirer.prompt(
+
+).then((response) =>{
+
+
+});
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    let readmeTemplate = ``;
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
