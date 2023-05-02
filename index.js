dc461956a18927cc11228ddda20dbded61e42cf5
KyleM021199 <kylem021199@gmail.com>
2023-05-02 13:30:37 -0500

    completed renderLicenseBadge function

--- index.js
+++ index.js
@@ -1,58 +1,87 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+const writeREADME = require('./generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 const questions = ["What is your Repo name?", 
-"Enter your description", 
-"Enter install instructions",
-"Enter Usage information", 
-"Enter contribution guidelines", 
-"Choose a repo license",
-"Enter test instructions",
-"Enter Github name",
-"Enter email address"];
+"Enter your description:", 
+"Enter install instructions:",
+"Enter Usage information:", 
+"Enter contribution guidelines:", 
+"Choose a repo license:",
+"Enter test instructions:",
+"Enter Github name:",
+"Enter email address:"];
 
-inquirer.prompt([
-
-    {
-      type: 'input',
-      message: questions[0],
-      name: 'name',
-    },
-    {
-      type: 'input',
-      message: questions[1],
-      name: 'location',
-    },
-    {
-      type: 'input',
-      message: questions[2],
-      name: 'github',
-    },
-    {
-    type: 'input',
-    message: questions[3],
-    name: 'linkedIn',
-
-    },
-  ]).then((response) =>{
-const fileName = "README.md"
-const writeREADME = require('./generateMarkdown.js') 
-
-    writeToFile(fileName, writeREADME)
-
-});
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-
-    fs.writeFile(fileName,data)
+    const fileName = "README.md";       
+    fs.writeFile(fileName,generateMarkdown(data), err =>{});
+    
 }
 
 // TODO: Create a function to initialize app
 function init() {
+    let answers= [];
+inquirer.prompt([
+    {
+      type: 'input',
+      message: questions[0],
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: questions[1],
+      name: 'description',
+    },
+    {
+      type: 'input',
+      message: questions[2],
+      name: 'installInst',
+    },
+    {
+    type: 'input',
+    message: questions[3],
+    name: 'usageInfo',
 
+    },
+    {
+    type: 'input',
+    message: questions[4],
+    name: 'contriGuide',
+    
+    },
+    {
+    type: 'checkbox',
+    message: questions[5],
+    name: 'license',
+    choices:['MIT', 'ISC', 'Apache 2.0']
+        
+    },
+    {
+    type: 'input',
+    message: questions[6],
+    name: 'testInstr',
+            
+    },
+    {
+    type: 'input',
+    message: questions[7],
+    name: 'github',
+                
+    },
+    {
+    type: 'input',
+    message: questions[8],
+    name: 'email',
+        
+    },
+  ]).then((response) =>{
+answers.push(['']);
+
+});
 
 }
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,60 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
+if(license == ""){
+return ``;
+}
+if(license == "MIT"){
+  return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
 
+}else if(license == "ISC"){
+  return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`;
+
+}else if(license == "Apache 2.0"){
+  return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+}
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license == ""){
+
+  }
+  if(license == "MIT"){
+  
+  }else if(license == "ISC"){
+  
+    
+  }else if(license == "Apache 2.0"){
+  
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license == ""){
+
+  }
+  if(license == "MIT"){
+  
+  }else if(license == "ISC"){
+  
+    
+  }else if(license == "Apache 2.0"){
+  
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+  ## Description
+  ${data.description}
+
+  ##
 
 `;
 }
